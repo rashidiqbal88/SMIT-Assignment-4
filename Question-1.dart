cadf72eb1f5cf4f5cd4594a54dc5395bf130be86
rashidiqbal88 <rashidiqbal.developer@gmail.com>
2024-02-09 23:48:32 +0500

    Question 1

---
+++ Question-1.dart
@@ -0,0 +1,11 @@
+void main() {
+  print(
+      "Q1: Write a program that takes a list of numbers as input and prints the even numbers in the list using a for loop.");
+  List<int> evenumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  for (int i = 1; i < evenumber.length; i++) {
+    if (i % 2 == 0) {
+      print("Even Number is:$i");
+    }
+  }
+}
