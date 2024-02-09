fabbfa1c93568ade64c492050de7ec7055499fc4
rashidiqbal88 <rashidiqbal.developer@gmail.com>
2024-02-09 23:46:56 +0500

    Question 7 update

---
+++ Question-7.dart
@@ -0,0 +1,10 @@
+void main() {
+  print(
+      "Q7: Write a program that prints the multiplication table of a given number using a for loop.");
+
+  int number = 5;
+
+  for (var i = 0; i <= 10; i++) {
+    print("$number x $i = ${i * number}");
+  }
+}
