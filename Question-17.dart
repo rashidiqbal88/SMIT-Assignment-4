f2cdcb5ee129f94495a5903ba5caa2ec548c163b
rashidiqbal88 <rashidiqbal.developer@gmail.com>
2024-02-10 23:51:53 +0500

    update

--- Question-17.dart
+++ Question-17.dart
@@ -1,21 +1,18 @@
 import 'dart:io';
-void main(){
+
+void main() {
   var email = stdin.readLineSync();
   var password = stdin.readLineSync();
   bool isAdminlogin = true;
 
-  while(isAdminlogin == true){
-
-    if(email == "admin@gmail.com" && password == "admin123"){
-      
+  while (isAdminlogin == true) {
+    if (email == "admin@gmail.com" && password == "admin123") {
       print("Login Successfully...");
       isAdminlogin = false;
-
-    }else{
-
+    } else {
       print("Please Try again...");
-
+      email = stdin.readLineSync();
+      password = stdin.readLineSync();
     }
-  
   }
 }
